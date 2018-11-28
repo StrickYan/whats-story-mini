eb53419bf533e3b7109bff8594056c54346c81b7
yanjing <yanjing@do-global.com>
2018-11-28 17:07:54 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,13 +15,13 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
